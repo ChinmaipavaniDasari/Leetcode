6f8a10ba48534cc8c75f90761fb99d1276e95135
ChinmaipavaniDasari <165761756+ChinmaipavaniDasari@users.noreply.github.com>
2025-07-29 13:21:05 +0530

    Time: 28 ms (48.65%), Space: 61.5 MB (67.57%) - LeetHub

---
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] smallestSubarrays(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int[] last = new int[32];  
+
+        for (int b = 0; b < 32; b++) {
+            last[b] = -1;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int b = 0; b < 32; b++) {
+                if ((nums[i] & (1 << b)) != 0) {
+                    last[b] = i;
+                }
+            }
+
+            int farthest = i;
+            for (int b = 0; b < 32; b++) {
+                if (last[b] != -1) {
+                    farthest = Math.max(farthest, last[b]);
+                }
+            }
+
+            ans[i] = farthest - i + 1;
+        }
+        return ans;
+    }
+}
